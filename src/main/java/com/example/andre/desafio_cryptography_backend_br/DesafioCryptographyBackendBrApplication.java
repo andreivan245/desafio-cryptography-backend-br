f0a9d4fb1a8ab73cb176529537417c6257815a2f
andreivan245 <andreflus887@gmail.com>
2024-10-01 17:36:23 -0300

    add initial project

---
+++ src/main/java/com/example/andre/desafio_cryptography_backend_br/DesafioCryptographyBackendBrApplication.java
@@ -0,0 +1,13 @@
+package com.example.andre.desafio_cryptography_backend_br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCryptographyBackendBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCryptographyBackendBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/andre/desafio_cryptography_backend_br/DesafioCryptographyBackendBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andre.desafio_cryptography_backend_br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCryptographyBackendBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
