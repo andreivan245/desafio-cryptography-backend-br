a12f08fe977574f5c4251e014e4c3ad25df8182f
andreivan245 <andreflus887@gmail.com>
2024-10-01 18:41:06 -0300

    feat: added UserRepository

--- src/main/java/com/example/andre/desafio_cryptography_backend_br/repository/UserRepository.java
+++ src/main/java/com/example/andre/desafio_cryptography_backend_br/repository/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CryptographyRepository extends JpaRepository<,ID> {
+public interface UserRepository extends JpaRepository<User,ID> {
 }
